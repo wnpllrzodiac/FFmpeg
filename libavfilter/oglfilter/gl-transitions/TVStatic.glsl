6d9a55e9f5c0b3a0b62352c662bb027d35b2129f
Michael.Ma <wnpllr@gmail.com>
2021-05-21 18:13:02 +0800

    fix TVStatic.glsl error: syntax error, unexpected NEW_IDENTIFIER, expecting $end highp

--- libavfilter/oglfilter/gl-transitions/TVStatic.glsl
+++ libavfilter/oglfilter/gl-transitions/TVStatic.glsl
@@ -4,13 +4,13 @@ uniform float offset; // = 0.05
 
 // Pseudo-random noise function
 // http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
-highp float noise(vec2 co)
+float noise(vec2 co)
 {
-    highp float a = 12.9898;
-    highp float b = 78.233;
-    highp float c = 43758.5453;
-    highp float dt= dot(co.xy * progress, vec2(a, b));
-    highp float sn= mod(dt,3.14);
+    float a = 12.9898;
+    float b = 78.233;
+    float c = 43758.5453;
+    float dt= dot(co.xy * progress, vec2(a, b));
+    float sn= mod(dt,3.14);
     return fract(sin(sn) * c);
 }
 
