00afeaf5486784252058d7faec6488dde1d0ce2c
Michael.Ma <guo.liang.ma@autodesk.com>
2023-08-14 16:41:23 +0800

    add bggen(open/close cam) filter

---
+++ libavfilter/oglfilter/gl-transitions/openandclose.glsl
@@ -0,0 +1,12 @@
+// simulate camera up/down open and close effect
+
+vec4 transition(vec2 uv) {
+  vec2 p=uv.xy/vec2(1.0).xy;
+  vec4 a=getFromColor(p);
+  vec4 b=getToColor(p);
+  //return mix(a, b, step(1.0-p.y,progress));
+  if (p.y < 0.75 + 0.125 * (1+cos(3.1415*4*progress)) && p.y > 0.25 - 0.125 * (1+cos(3.1415*4*progress)))
+    return a;
+  else
+    return b;
+}
